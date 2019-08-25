3e207b68c227852c08c232a3c519b3ce5f12c29b
Edwin@T480s <edwin.cb.tang@outlook.com>
2019-08-25 23:07:51 +0800

    Site updated: 2019-08-25 23:07:49

--- js/hideNseek.js
+++ js/hideNseek.js
@@ -3,15 +3,15 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/img/TEP.ico");
+        $('[rel="icon"]').attr('href', "/images/crash.ico");
         document.title = '页面崩溃啦！ Σ(° △ °|||)';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "/images/favicon-16x16.png");
         document.title = '突然又好了~ (*/ω\\*)';
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
+            $('[rel="icon"]').attr('href', "/images/favicon-16x16.png");
         }, 2000);
     }
 });
