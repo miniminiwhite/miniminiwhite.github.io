c219f2e216b3abc42ebfeeacc1596732a7505c87
Edwin@T480s <edwin.cb.tang@outlook.com>
2019-08-26 23:34:52 +0800

    Site updated: 2019-08-26 23:34:47

--- js/hideNseek.js
+++ js/hideNseek.js
@@ -1,17 +1,26 @@
 <!--崩溃欺骗-->
 var OriginTitle = document.title;
 var titleTime;
+var switched = false;
+
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/images");
-        document.title = '页面崩溃啦！ Σ(° △ °|||)';
-        clearTimeout(titleTime);
+        if (Math.random() > 0.7) {
+            clearTimeout(titleTime);
+            titleTime = setTimeout(function () {
+                $('[rel="icon"]').attr('href', "/images");
+                document.title = '页面崩溃啦！ Σ(° △ °|||)';
+                clearTimeout(titleTime);
+            }, 3000);
+            switched = true;
+        }
     }
-    else {
+    else if (switched) {
         document.title = '突然又好了~ (*/ω\\*)';
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
             $('[rel="icon"]').attr('href', "/images/favicon-16x16.png");
-        }, 1500);
+        }, 1000);
+        switched = false;
     }
 });
